0e5a93a4282e4665700a2770ef2bcc0c5c13d8f5
Ana <101293323+anaBequiman@users.noreply.github.com>
2022-09-05 16:38:28 -0300

    Update conversa.js

---
+++ conversa.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBox1We378uZDYfh8LZNxeQLkV6Ro7CvNM",
+    authDomain: "talk-997f7.firebaseapp.com",
+    databaseURL: "https://talk-997f7-default-rtdb.firebaseio.com",
+    projectId: "talk-997f7",
+    storageBucket: "talk-997f7.appspot.com",
+    messagingSenderId: "781676367890",
+    appId: "1:781676367890:web:3c84440e2008fa13fb08cb"
+  };
+// Initializing Frebase
+firebase.InitializeApp(firebaseConfig);
